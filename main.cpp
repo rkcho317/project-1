7ea8843bbb684b69467860e0bc118d1a11fd10b0
rkcho317 <rkcho317@csu.fullerton.edu>
2022-02-14 21:26:47 -0800

    segfault on friend vector3d operator*(T k, const vector3d& v) { return k*v;} ;

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@
 
 #define _USE_MATH_DEFINES 
 #include <cmath>
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+#ifndef M_PI_2
+    #define M_PI_2 3.14159265358979323846
+#endif
 
 
 template <typename T>
